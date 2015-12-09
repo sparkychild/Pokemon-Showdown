d49eaa2c829aa28951f93ad323d28f11c386b42e
Wando94 <daniel.wilson11@outlook.com>
2015-12-09 18:16:44 +0000

    Update tpoll.js

--- chat-plugins/tpoll.js
+++ chat-plugins/tpoll.js
@@ -1,6 +1,6 @@
 exports.commands = {
         tierpoll: 'tpoll',
         tpoll: function (target, room, user) {
-                return this.parse('/poll new Tournament Tier?, Anything Goes, Challenge Cup 1v1, Monotype, OU, Random Battle, Random Monotype Battle, VGC, UU, RU, NU, PU, LC, Ubers, OMM, Eights, Gen One Random, Capture and Evolve, Hackmons');
+                return this.parse('/poll new Tournament Tier?, Anything Goes, Challenge Cup 1v1, Monotype, OU, Random Battle, UU - LC, Eights/Capture and Evolve, Gen One Random/OMM');
         }
 }
