4156f843e794229d4ead359ffa15d7fd7e006175
BTCantGetBurns <mynameistanveeer@gmail.com>
2015-12-12 11:43:28 -0500

    Update formats.js

--- config/formats.js
+++ config/formats.js
@@ -3664,7 +3664,6 @@ exports.Formats = [
 		desc: [
 			"Pok&eacute;mon below OU/BL get all their stats boosted. UU/BL2 get +5, RU/BL3 get +10, NU/BL4 get +15, and PU or lower get +20.",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3554765/\">Tier Shift</a>"
-			"Pok&eacute;mon below OU/BL get all their stats boosted. UU/BL2 get +5, RU/BL3 get +10, and NU or lower get +15.",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3532973/\">Tier Shift</a>",
 			"&bullet; <a href=\"https://www.smogon.com/forums/threads/3536719/\">Tier Shift Viability Ranking</a>"
 		],
