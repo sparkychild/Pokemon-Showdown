f6bdb16372fb0713721a9799e10c5760c334661f
BTCantGetBurns <mynameistanveeer@gmail.com>
2015-12-20 14:23:05 -0500

    Update stafflist.js

--- chat-plugins/stafflist.js
+++ chat-plugins/stafflist.js
@@ -4,10 +4,10 @@ exports.commands = {
 	globalauth: 'authority',
 	authlist: 'authority',
 	authority: function (target, room, user, connection) {
-		let rankLists = {};
-		let ranks = Object.keys(Config.groups);
-		for (let u in Users.usergroups) {
-			let rank = Users.usergroups[u].charAt(0);
+		var rankLists = {};
+		var ranks = Object.keys(Config.groups);
+		for (var u in Users.usergroups) {
+			var rank = Users.usergroups[u].charAt(0);
 			if (rank === ' ' || rank === '+') continue;
 			// In case the usergroups.csv file is not proper, we check for the server ranks.
 			if (ranks.indexOf(rank) >= 0) {
