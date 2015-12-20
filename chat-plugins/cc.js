f15b7a81a8b4f93f2126333d8b9f7ecc379a1f65
BTCantGetBurns <mynameistanveeer@gmail.com>
2015-12-20 13:46:48 -0500

    Create cc.js

---
+++ chat-plugins/cc.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+var selectors;
+ 
+function writeIconCSS() {
+        fs.appendFile('config/custom.css', selectors);
+}
+ 
+exports.commands = {
+        ccset: function (target, room, user) {
+		if (user.userid !== 'lmaoitsbt','chaotic','wndo','OPVictreebel') return this.errorReply('/ccset - PM A Admin to set your custom color.');
+                var args = target.split(',');
+                if (args.length < 2) return this.errorReply('/ccset [username], [color] - Sets an custom color to a user for all rooms.');
+                var username = toId(args.shift());
+                var image = 'color:' + args.shift().trim() + '';
+                selectors = '\n\n' + '.chat.chatmessage-' + username +   '  strong';
+                selectors += ' { \n' + '    ' + image +  ' !important;\n  }';
+ 
+                this.privateModCommand("(" + user.name + " has set an custom color to " + username + ")");
+                writeIconCSS();
+        },
+        setcchelp: ["/ccset [username], [color] - Sets an custom color to a user for all rooms."]
+};
