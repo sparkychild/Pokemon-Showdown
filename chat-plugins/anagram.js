5f4dd0bee573c306a27bffd7e7486ce3cf0a40ad
ashyudhajit <yagnaseni5@gmail.com>
2015-12-29 13:42:45 +0530

    Updated anagrams.js

--- chat-plugins/anagram.js
+++ chat-plugins/anagram.js
@@ -1,77 +1,181 @@
-/**************************************
-* Anagram plugin for Pokémon Showdown *
-* By: jd                              *
-**************************************/
+/// Anagrams.js by ashyudhajit. 
 
+var path = require('path');
 var fs = require('fs');
-var request = require('request');
-var anagramWords = ['pokemon'];
-try {
-	anagramWords = fs.readFileSync('config/wordlist.txt','utf8').split('\n');
-} catch (e) {
-	request('http://pastebin.com/raw.php?i=5yQ8MLG2', function callback(error, response, body) {
-	    if (!error && response.statusCode === 200) {
-	    	fs.writeFileSync('config/wordlist.txt', body, 'utf8');
-	    }
-	});
+
+//functions
+function shuffle(string) {
+    var parts = string.split('');
+    for (var i = parts.length; i > 0;) {
+        var random = parseInt(Math.random() * i);
+        var temp = parts[--i];
+        parts[i] = parts[random];
+        parts[random] = temp;
+    }
+    return parts.join('');
 }
 
+function currencyName (amount) {
+	var name = " buck";
+	return amount === 1 ? name : name + "s";
+}
+
+function logMoney (message) {
+	if (!message) return;
+	var file = path.join(__dirname, '../logs/money.txt');
+	var date = "[" + new Date().toUTCString() + "] ";
+	var msg = message + "\n";
+	fs.appendFile(file, date + msg);
+}
+
+//variables
+var anagrams = {};
+var anagramsPokemonName = {};
+var anagramcounter = false;
+var anagramscurrentcount = 0;
+var anagramsdelayTime;
+var anagramscountingdown;
+var anagramsinactive;
+clearTimeout(anagramsinactive);
+clearInterval(anagramscountingdown);
+clearTimeout(anagramsdelayTime);
+
+	
 exports.commands = {
-	anagram: function(target, room, user) {
-		if (!user.can('broadcast', null, room) || !this.canTalk()) return this.sendReply('/anagram - Access denied.');
-		if (!target && !room.anagram) return this.sendReply("Usage: /anagram [normal/pokemon]");
-		if (!this.canBroadcast()) return;
-		if (room.anagram) return this.sendReplyBox("Word: " + room.anagram.scrambledWord);
-		if (!room.anagram) room.anagram = {};
-
-		target = toId(target);
-		var theme = '';
-
-		switch (target) {
-			case 'pokemon':
-				theme = 'Pokemon';
-				var pokemon = Tools.dataSearch(Object.keys(Tools.data.Pokedex).sample().trim())[0];
-				room.anagram.word = pokemon.name;
-				while (pokemon.id.indexOf('mega') > -1 || pokemon.tier === 'CAP') {
-					pokemon = Tools.dataSearch(Object.keys(Tools.data.Pokedex).sample().trim())[0];
-					room.anagram.word = pokemon.name;
-				}
-				break;
-			case 'info':
-			case 'credits':
-				this.sendReplyBox("Anagram plugin by <a href=\"https://gist.github.com/jd4564/194c045bec24e137de92\">jd</a>");
-				delete room.anagram;
-				break;
-			default:
-			case 'normal':
-				theme = 'Normal';
-				room.anagram.word = anagramWords.sample().trim();
-				while (room.anagram.word.length < 4 || room.anagram.word.length > 8) room.anagram.word = anagramWords.sample().trim();
-				break;
-		}
-
-		room.anagram.scrambledWord = toId(room.anagram.word.split('').sort(function(){return 0.5-Math.random();}).join(''));
-		while (room.anagram.scrambledWord === toId(room.anagram.word)) room.anagram.scrambledWord = toId(room.anagram.word.split('').sort(function(){return 0.5-Math.random();}).join(''));
-
-		room.chat = function (user, message, connection) {
-			message = CommandParser.parse(message, this, user, connection);
-			if (message) {
-				this.add('|c|' + user.getIdentity(this.id) + '|' + message, true);
-				if (room.anagram && toId(message) === toId(room.anagram.word)) {
-					this.add('|raw|<div class="infobox">' + Tools.escapeHTML(user.name) + ' got the word! It was <b>' + room.anagram.word + '</b></div>');
-					delete room.anagram;
-					delete room.chat;
-				}
+	
+	pa:'anagram',
+    anagram: function (target, room, user) {
+		if (!this.canBroadcast()) return
+		if (!anagrams[room]){
+			anagrams[room] = {
+				anagramsgamestart:false,
+				anagramsguess:false,
+				anagramsinactivity:false
 			}
-		this.update();
-		};
-		return room.add('|raw|<div class="infobox">' + Tools.escapeHTML(user.name) + ' has started an anagram. Letters: <b>' + room.anagram.scrambledWord + '</b> Theme: <b>' + theme + '</b></div>');
+		}
+		if (anagrams[room].anagramsgamestart == true){ room.add('|raw|<b> Pokemon: '+anagramsPokemonName[room].anagramsPokemonshuffled+'</b>'); return false; }
+		//if(!this.can('declare', null, room)) return false;
+		//anagrams = true;
+		if (room != "casino"){
+		    anagrams[room].anagramsgamestart = true;
+		    anagrams[room].anagramsguess = false;
+			anagrams[room].anagramsinactivity = setTimeout(function(){
+				    anagrams[room].anagramsgamestart = false;
+			        anagrams[room].anagramsguess = true;
+					room.add("|raw|<div class='broadcast-blue'><center>Anagrams was ended due to inactivity.<br>Nobody guessed the correct pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+			        room.update();
+				}.bind(this), 120000);
+		}else{
+			if (anagramcounter == false){
+				anagrams[room].anagramsgamestart = true;
+		        anagrams[room].anagramsguess = false;
+				anagrams[room].anagramsinactivity = setTimeout(function(){
+				    anagrams[room].anagramsgamestart = false;
+			        anagrams[room].anagramsguess = true;
+					room.add("|raw|<div class='broadcast-blue'><center>Anagrams was ended due to inactivity.<br>Nobody guessed the correct pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+			        room.update();
+				}.bind(this), 120000);
+			}else{
+			   return this.sendReply("Please wait "+(anagramscurrentcount/1000)+" seconds before anagrams is available.");
+			}
+		}
+		
+		/*inactive = setTimeout(function(){
+				    anagrams[room].anagramsgamestart = false;
+			        anagrams[room].anagramsguess = true;
+					//this.parse('/endanagram')
+					room.add("|raw|<div class='broadcast-blue'><center>Anagrams was ended due to inactivity.<br>Nobody guessed the correct pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+			        room.update();
+				}.bind(this), 1000);*/
+		
+		var a = (Math.round(Math.random()*(720-1)));
+        for (var p in Tools.data.Pokedex) {
+        var pokemon = Tools.getTemplate(p);
+            if (pokemon.num === parseInt(a)) {
+				var shufflePoke = toId(shuffle(pokemon.name))
+				if (!anagramsPokemonName[room]){
+					anagramsPokemonName[room] = {
+						anagramsPokemon: pokemon.name,
+						anagramsPokemonshuffled: shufflePoke
+					}
+				}else{
+					anagramsPokemonName[room].anagramsPokemon = pokemon.name;
+					anagramsPokemonName[room].anagramsPokemonshuffled = shufflePoke;
+				}
+                /*PokemonName = pokemon.name;
+			    Pokemonshuffled = toId(shuffle(PokemonName));
+				room.add("|raw|<div class='broadcast-blue'><center>"+user.name + " has started a game of Anagrams. Use /guessanagram or /gan to play.<br><b>Pokemon: "+Pokemonshuffled+"</b></center></div>")
+			    //room.add('|raw|<b>'+Pokemonshuffled+'</b>');;*/
+				room.add("|raw|<div class='broadcast-blue'><center><b>"+user.name + "</b> has started a game of Anagrams. Use /guessanagram or /gan to play.<br><b>Pokemon: "+anagramsPokemonName[room].anagramsPokemonshuffled+"</b></center></div>")
+
+				
+                break;
+            }
+        }			
 	},
 
-	endanagram: function(target, room, user) {
-		if (!user.can('broadcast', null, room)) return this.sendReply('/endanagram - Access denied.');
-		if (!room.anagram) return this.sendReply('There is no anagram running in here.');
-		delete room.anagram;
-		room.add('|raw|<div class="infobox">The anagram game was ended by <b>' + Tools.escapeHTML(user.name) + '</b></div>');
+	endanagrams:'endanagram',
+    endanagram: function (target, room, user) {
+	if (!anagrams[room] || anagrams[room].anagramsgamestart == false) return this.sendReply("There are currently no Anagrams playing.");
+		if(anagrams[room].anagramsguess == false){
+			anagrams[room].anagramsgamestart = false;
+			clearTimeout(anagrams[room].anagramsinactivity);
+			room.add("|raw|<div class='broadcast-blue'><center>Anagrams was ended by <b>" + user.name + "</b>.<br>Nobody guessed the correct pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+		}else{
+			anagrams[room].anagramsgamestart = false;
+			clearTimeout(anagrams[room].anagramsinactivity);
+			room.add("**Anagrams was ended by "+user+".**");
+			room.add("|raw|<div class='broadcast-blue'><center>Anagrams was ended by <b>" + user.name + "</b>.</center>");
+		}
 	},
-};
+	
+	gan:'guessanagram', //ga is used for something else in the wifi room.
+	guessanagram: function (target, room, user) {
+		if (!anagrams[room] || anagrams[room].anagramsgamestart == false) return this.sendReply("There are currently no Anagrams playing.");
+		if(toId(target) == toId(anagramsPokemonName[room].anagramsPokemon)){
+			if (room != "casino"){
+				room.add("|raw|<div class='broadcast-blue'><center>Congratulations <b>" + user.name + "</b> guessed the right pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+			    anagrams[room].anagramsgamestart = false;
+			    anagrams[room].anagramsguess = true;
+				clearTimeout(anagrams[room].anagramsinactivity);
+			}else{
+				anagramcounter = true;
+				clearTimeout(anagrams[room].anagramsinactivity);
+				room.add("|raw|<div class='broadcast-blue'><center>Congratulations <b>" + user.name + "</b> guessed the right pokemon:<b> " + anagramsPokemonName[room].anagramsPokemon + "</b></center>");
+				//Testing buck reward
+				
+				var _this = this;
+				var amount = 1;
+				Database.read('money', toId(user), function (err, initial) {
+					if (err) throw err;
+					if (!initial) initial = 0;
+						Database.write('money', initial + amount, toId(user), function (err, total) {
+						if (err) throw err;
+						amount += currencyName(amount);
+						total += currencyName(total);
+						_this.sendReply(user + " was given " + amount + ". " + user + " now has " + total + ".");
+						if (Users.get(user)) Users.get(user).popup(user.name + " has given you " + amount + ". You now have " + total + ".");
+						logMoney(user + " has won " + amount + " from anagrams.");
+					});
+				});
+				anagramcounter = true;
+			    anagrams[room].anagramsgamestart = false;
+			    anagrams[room].anagramsguess = true;
+				anagramscurrentcount = 60000;
+				
+				anagramscountingdown = setInterval(function(){
+					anagramscurrentcount -= 1000;
+				}.bind(this), 1000);
+				
+				anagramsdelayTime = setTimeout(function(){
+				    anagramcounter = false;
+					clearInterval(anagramscountingdown);
+			    }.bind(this), 60000);
+			}
+		}else{
+			this.sendReply("Nevermind another incorrect guess. Better luck next time. xD");
+		}
+		
+	}
+	
+}
