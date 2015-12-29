17aa09dffca206543478f24f1ac81718c11be883
BTCantGetBurns <mynameistanveeer@gmail.com>
2015-12-29 16:19:51 -0500

    Update emoticon.js

--- chat-plugins/emoticon.js
+++ chat-plugins/emoticon.js
@@ -1,10 +1,10 @@
 'use strict';
 
-let color = require('../config/color');
+var color = require('../config/color');
 
 exports.parseEmoticons = parseEmoticons;
 
-let emotes = {
+var emotes = {
 	'#freewolf': 'http://i.imgur.com/ybxWXiG.png',
 	'feelsbd': 'http://i.imgur.com/YyEdmwX.png',
 	'feelsbm': 'http://i.imgur.com/xwfJb2z.png',
@@ -37,16 +37,16 @@ let emotes = {
 	'yesface': 'http://i.imgur.com/k9YCF6K.png'
 };
 
-let emotesKeys = Object.keys(emotes);
-let patterns = [];
-let metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
+var emotesKeys = Object.keys(emotes);
+var patterns = [];
+var metachars = /[[\]{}()*+?.\\|^$\-,&#\s]/g;
 
-for (let i in emotes) {
+for (var i in emotes) {
 	if (emotes.hasOwnProperty(i)) {
 		patterns.push('(' + i.replace(metachars, '\\$&') + ')');
 	}
 }
-let patternRegex = new RegExp(patterns.join('|'), 'g');
+var patternRegex = new RegExp(patterns.join('|'), 'g');
 
 /**
  * Parse emoticons in message.
@@ -60,8 +60,8 @@ let patternRegex = new RegExp(patterns.join('|'), 'g');
 function parseEmoticons(message, room, user, pm) {
 	if (typeof message !== 'string' || (!pm && room.disableEmoticons)) return false;
 
-	let match = false;
-	let len = emotesKeys.length;
+	var match = false;
+	var len = emotesKeys.length;
 
 
 	while (len--) {
@@ -78,7 +78,7 @@ function parseEmoticons(message, room, user, pm) {
 
 	// add emotes
 	message = message.replace(patternRegex, function (match) {
-		let emote = emotes[match];
+		var emote = emotes[match];
 		return typeof emote === 'string' ? '<img src="' + emote + '" title="' + match + '" height="50" width="50" />' : match;
 	});
 
@@ -88,10 +88,10 @@ function parseEmoticons(message, room, user, pm) {
 	// **bold**
 	message = message.replace(/\*\*([^< ](?:[^<]*?[^< ])?)\*\*/g, '<b>$1</b>');
 
-	let group = user.getIdentity().charAt(0);
+	var group = user.getIdentity().charAt(0);
 	if (room.auth) group = room.auth[user.userid] || group;
 
-	let style = "background:none;border:0;padding:0 5px 0 0;font-family:Verdana,Helvetica,Arial,sans-serif;font-size:9pt;cursor:pointer";
+	var style = "background:none;border:0;padding:0 5px 0 0;font-family:Verdana,Helvetica,Arial,sans-serif;font-size:9pt;cursor:pointer";
 
 	message = "<div class='chat'>" + "<small>" + group + "</small>" + "<button name='parseCommand' value='/user " + user.name + "' style='" + style + "'>" + "<b><font color='" + color(user.userid) + "'>" + user.name + ":</font></b>" + "</button><em class='mine'>" + message + "</em></div>";
 	if (pm) return message;
@@ -107,11 +107,11 @@ function parseEmoticons(message, room, user, pm) {
  * @return {String} emotes table
  */
 function create_table() {
-	let emotes_name = Object.keys(emotes);
-	let emotes_list = [];
-	let emotes_group_list = [];
-	let len = emotes_name.length;
-	let i;
+	var emotes_name = Object.keys(emotes);
+	var emotes_list = [];
+	var emotes_group_list = [];
+	var len = emotes_name.length;
+	var i;
 
 	for (i = 0; i < len; i++) {
 		emotes_list.push("<td>" +
@@ -119,7 +119,7 @@ function create_table() {
 			emotes_name[i] + "</td>");
 	}
 
-	let emotes_list_right = emotes_list.splice(len / 2, len / 2);
+	var emotes_list_right = emotes_list.splice(len / 2, len / 2);
 
 	for (i = 0; i < len / 2; i++) {
 		let emote1 = emotes_list[i],
@@ -134,7 +134,7 @@ function create_table() {
 	return "<div class='infobox'><center><b><u>List of Emoticons</u></b></center>" + "<div class='infobox-limited'><table border='1' cellspacing='0' cellpadding='5' width='100%'>" + "<tbody>" + emotes_group_list.join("") + "</tbody>" + "</table></div></div>";
 }
 
-let emotes_table = create_table();
+var emotes_table = create_table();
 
 exports.commands = {
 	blockemote: 'blockemoticons',
@@ -181,8 +181,8 @@ exports.commands = {
 	rande: 'randemote',
 	randemote: function (target, room, user) {
 		if (!this.canBroadcast()) return;
-		let rng = Math.floor(Math.random() * emotesKeys.length);
-		let randomEmote = emotesKeys[rng];
+		var rng = Math.floor(Math.random() * emotesKeys.length);
+		var randomEmote = emotesKeys[rng];
 		this.sendReplyBox("<img src='" + emotes[randomEmote] + "' title='" + randomEmote + "' height='50' width='50' />");
 	},
 	randemotehelp: ["/randemote - Get a random emote."]
