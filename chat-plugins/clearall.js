f25fdf2b01ce5198a600ed310950b5cb4a50acd4
BTCantGetBurns <mynameistanveeer@gmail.com>
2015-12-31 18:26:54 -0500

    Create clearall.js

---
+++ chat-plugins/clearall.js
@@ -0,0 +1,21 @@
+exports.commands = {
+clearall: function (target, room, user) {
+		if (!this.can('declare')) return false;
+		if (room.battle) return this.sendReply("You cannot clearall in battle rooms.");
+
+		clearRoom(room);
+	},
+
+	gclearall: 'globalclearall',
+	globalclearall: function (target, room, user) {
+		if (!this.can('gdeclare')) return false;
+
+		for (let u in Users.users) {
+			Users.users[u].popup("All rooms are being clear.");
+		}
+
+		for (let r in Rooms.rooms) {
+			clearRoom(Rooms.rooms[r]);
+		}
+	},
+}
