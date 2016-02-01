87fa88037e444347029c0f94a163b9e34b506352
Wando94 <daniel.wilson11@outlook.com>
2016-02-01 22:33:29 +0000

    Update commands.js

--- commands.js
+++ commands.js
@@ -19,7 +19,7 @@ var fs = require('fs');
 const MAX_REASON_LENGTH = 300;
 const MUTE_LENGTH = 7 * 60 * 1000;
 const HOURMUTE_LENGTH = 60 * 60 * 1000;
-const DAYMUTE_LENGTH = 1440;
+const DAYMUTE_LENGTH = 1440 * 1440 * 2000;
 
 var commands = exports.commands = {
 	
