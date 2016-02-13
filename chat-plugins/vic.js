e0599ec643c9fe3b48abfc262955d96f895048a3
Wando94 <daniel.wilson11@outlook.com>
2016-02-13 00:02:58 +0000

    Create vic.js

---
+++ chat-plugins/vic.js
@@ -0,0 +1,5 @@
+exports.commands = {
+  	v: 'vic',
+  		popup("<html><div style="background-image: &quot;http://static.spin.com/files/141201-r%C3%B6yksopp-skulls-music-video-watch-cabin-rave-creepy.png&quot;); background-size: 100% 100%" border="0" width="100%"><center><img src="http://images.cooltext.com/4547425.png" width="350" height="70" alt="OPVictreebel" /><br></center><br><center><img src="http://www.screwattack.com/sites/default/files/jigglypuff.gif" height="" width=""><br><img src="http://images.cooltext.com/4547442.png" width="197" height="103" alt="'Run!'" /><audio src="https://api.soundcloud.com/tracks/177061189/stream?client_id=02gUJC0hH2ct1EGOcYXQIzRFU91c72Ea" controls="" style="width: 99% ; border: 2px solid #ffffFF ; background-color: #ffffff" target="_blank"></audio></div></html>");
+  	}
+  };
